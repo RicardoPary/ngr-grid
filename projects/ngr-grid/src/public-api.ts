a777c233abed7e6ef71d40c2ec95610bb18e9e3c
Ricardo Pari <rikiricardo16@hotmail.com>
2021-02-10 19:20:54 -0400

    version 1.0.2

--- projects/ngr-grid/src/public-api.ts
+++ projects/ngr-grid/src/public-api.ts
@@ -4,3 +4,7 @@
 
 export * from './lib/ngr-grid.module';
 export * from './lib/ngr-grid.component';
+
+// COMPONENTS
+export * from './lib/components/action-button.component';
+export * from './lib/components/actions-button-patient.component';
